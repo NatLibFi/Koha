bf63f670903cad0fc2f5d3ac03b04d34967e70e2
Andrew Nugged <nugged@gmail.com>
2021-10-15 12:21:55 +0300

    Bug 00000: POC PATCH. I'd like to have DRY RUN and VERBOSE features in shell scripts somewhere

--- misc/cronjobs/holds/build_holds_queue.pl
+++ misc/cronjobs/holds/build_holds_queue.pl
@@ -1,14 +1,26 @@
 #!/usr/bin/perl 
-#-----------------------------------
-# Script Name: build_holds_queue.pl
-# Description: builds a holds queue in the tmp_holdsqueue table
-#-----------------------------------
-# FIXME: add command-line options for verbosity and summary
-# FIXME: expand perldoc, explain intended logic
+
+# Copyright 2017 Koha Development team
+# Copyright Liblime 2008
+# Copyright 2021 University of Helsinki (The National Library Of Finland)
+#
+# This file is part of Koha.
+#
+# Koha is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# Koha is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Koha; if not, see <http://www.gnu.org/licenses>.
 
 use Modern::Perl;
-
-use Getopt::Long qw( GetOptions );
+use Getopt::Long qw( GetOptions :config no_ignore_case );
 use Pod::Usage qw( pod2usage );
 
 use C4::Context;
@@ -16,18 +28,65 @@ use C4::HoldsQueue qw(CreateQueue);
 use C4::Log qw( cronlogaction );
 use Koha::Script -cron;
 
+sub usage {
+    pod2usage( -exitval => 0, -verbose => 2 );
+    exit;
+}
+
+my $help  = 0;
+my $man   = 0;
+my $force = 0;
+my $dry_run;
+my $verbose;
+
+my $command_line_options = join( " ", @ARGV );
+
+GetOptions(
+    'h|?|help'   => \$help,
+    'm|man'      => \$man,
+    'f|force'    => \$force,
+    'v|verbose+' => \$verbose,
+    'n|dry-run'  => \$dry_run,
+) or usage();
+pod2usage(1) if $help;
+usage() if $man;
+
+my $rthq = C4::Context->preference('RealTimeHoldsQueue');
+
+if ( $rthq && !$force ) {
+    say "RealTimeHoldsQueue system preference is enabled, holds queue not built.";
+    say "Use --force to force building the holds queue.";
+    exit(1);
+}
+
+if ( $dry_run && $verbose ) {
+    print "Dry run!\n";
+} else {
+    cronlogaction();
+}
+
+CreateQueue({
+    verbose => $verbose,
+    dry_run => $dry_run,
+});
+
+unless ( $dry_run ) {
+    cronlogaction({ action => 'End', info => "COMPLETED" });
+}
+
 =head1 NAME
 
-build_holds_queue.pl - Rebuild the holds queue
+build_holds_queue.pl - builds a holds queue in the tmp_holdsqueue table
 
 =head1 SYNOPSIS
 
-build_holds_queue.pl [-f]
+  ./build_holds_queue.pl ...
 
- Options:
-   -h --help        Brief help message
-   -m --man         Full documentation
-   -f --force    Run holds queue builder even if RealTimeHoldsQueue is enabled
+=head1 DESCRIPTION
+
+This script calls C4::Reserves::CancelExpiredReserves which will find and cancel all expired reseves in the system.
+
+=cut
 
 =head1 OPTIONS
 
@@ -45,38 +104,14 @@ Prints the manual page and exits.
 
 Allows this script to rebuild the entire holds queue even if the RealTimeHoldsQueue system preference is enabled.
 
+=item B<--verbose|-v>
+
+Be verbose
+
+=item B<--dry-run|-n>
+
+Don't change data (dry-run)
+
 =back
 
-=head1 DESCRIPTION
-
-This script builds or rebuilds the entire holds queue.
-
 =cut
-
-my $help  = 0;
-my $man   = 0;
-my $force = 0;
-
-my $command_line_options = join( " ", @ARGV );
-
-GetOptions(
-    'h|help'  => \$help,
-    'm|man'   => \$man,
-    'f|force' => \$force,
-);
-pod2usage(1)                              if $help;
-pod2usage( -exitval => 0, -verbose => 2 ) if $man;
-
-my $rthq = C4::Context->preference('RealTimeHoldsQueue');
-
-if ( $rthq && !$force ) {
-    say "RealTimeHoldsQueue system preference is enabled, holds queue not built.";
-    say "Use --force to force building the holds queue.";
-    exit(1);
-}
-
-cronlogaction( { info => $command_line_options } );
-
-CreateQueue();
-
-cronlogaction( { action => 'End', info => "COMPLETED" } );
