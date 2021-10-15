ffdb2abb561f1ec1d335575a06d546508f7e8ba0
Andrew Nugged <nugged@gmail.com>
2021-10-16 01:16:16 +0300

    Bug 20447: (QA follow-up) Remove warning when frameworkcode is undef

--- cataloguing/addholding.pl
+++ cataloguing/addholding.pl
@@ -587,7 +587,7 @@ my ($template, $loggedinuser, $cookie) = get_template_and_user(
 my $record = $holding_id ? Koha::Holdings->find($holding_id) : Koha::Holding->new();
 
 $frameworkcode = $record->frameworkcode if ($record);
-$frameworkcode = 'HLD' if ($frameworkcode eq '');
+$frameworkcode = 'HLD' if not $frameworkcode or $frameworkcode eq '';
 
 # TODO: support in advanced editor?
 #if ( $op ne "delete" && C4::Context->preference('EnableAdvancedCatalogingEditor') && C4::Auth::haspermission(C4::Context->userenv->{id},{'editcatalogue'=>'advanced_editor'}) && $input->cookie( 'catalogue_editor_' . $loggedinuser ) eq 'advanced' ) {
