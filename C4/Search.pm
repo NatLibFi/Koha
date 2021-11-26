bce5b7a66a2418218a9dd48448dc3ad77f46b81a
Andrew Nugged <nugged@gmail.com>
2021-11-26 13:45:37 +0200

    Bug 00000: HOTFIX. Warn removal: transfertwhen in ne comes undef and, interesting: it screened two times before in lines above to prevent comparison of undef with '', but third time that missed to be checked, because new code added? Whatever, now all three places.
    ($transfertwhen, $transfertfrom, $transfertto) values get directly from
    SELECT datesent, frombranch, tobranch,
    SQL
    
    Use of uninitialized value $transfertwhen in string ne at .../C4/Search.pm line 1925.

--- C4/Search.pm
+++ C4/Search.pm
@@ -1967,7 +1967,7 @@ sub searchResults {
                         $other_items->{$key}->{$_} = $item->{$_};
                     }
                     $other_items->{$key}->{branchcode}     = $item->{branchcode};
-                    $other_items->{$key}->{intransit} = ( $transfertwhen ne '' ) ? 1 : 0;
+                    $other_items->{$key}->{intransit} = ( defined $transfertwhen && $transfertwhen ne '' ) ? 1 : 0;
                     $other_items->{$key}->{recalled} = ($recallstatus) ? 1 : 0;
                     $other_items->{$key}->{onhold} = ($reservestatus) ? 1 : 0;
                     $other_items->{$key}->{notforloan} = GetAuthorisedValueDesc('','',$item->{notforloan},'','',$notforloan_authorised_value) if $notforloan_authorised_value and $item->{notforloan};
