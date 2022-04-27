04eb214f2c0a842521e6609f0fadef9391ffcb70
Andrew Nugged <nugged@gmail.com>
2022-04-27 12:18:13 +0300

    Bug 00000: Plack transmits 200 code instead of 4xx/500 this makes HTTP-Auth layer if needed to work improperly

--- errors/401.pl
+++ errors/401.pl
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Koha; if not, see <http://www.gnu.org/licenses>.
 
+
 use Modern::Perl;
 use CGI qw ( -utf8 );
 use C4::Auth qw( get_template_and_user );
@@ -37,7 +38,7 @@ $template->param (
     errno => 401,
 );
 my $status = '401 Unauthorized';
-if ( any { /(^psgi\.|^plack\.)/i } keys %ENV ) {
-    $status = '200 OK';
-}
+# if ( any { /(^psgi\.|^plack\.)/i } keys %ENV ) {
+#     $status = '200 OK';
+# }
 output_with_http_headers $query, $cookie, $template->output, 'html', $status;
