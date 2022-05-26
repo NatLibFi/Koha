50f3acd7598b5f034f384eeb017db2055baa8b58
Andrew Nugged <nugged@gmail.com>
2022-05-27 00:33:02 +0300

    Bug 00000: race conditions fix for background worker:
    it getting args from RabbitMQ too fast, data still not yet in DB on that
    moment, so it fails with empty $job object:
    
    Can't call method "process" on an undefined value at /usr/share/koha/bin/background_jobs_worker.pl line 124.

--- misc/workers/background_jobs_worker.pl
+++ misc/workers/background_jobs_worker.pl
@@ -160,7 +160,16 @@ while (1) {
 
         # FIXME This means we need to have create the DB entry before
         # It could work in a first step, but then we will want to handle job that will be created from the message received
+        # sleep 2;
         my $job = Koha::BackgroundJobs->search( { id => $args->{job_id}, status => 'new' } )->next;
+        if(! $job) {
+            warn localtime().": Job $args->{job_id} not found, race conditions, sleeping 2 secs more:\n";
+            sleep 2;
+            $job = Koha::BackgroundJobs->search( { id => $args->{job_id}, status => 'new' } )->next;
+            if(! $job) {
+                die localtime().": Anyway failed to get job $args->{job_id}.\n";
+            }
+        }
 
         unless( $job ) {
             Koha::Logger->get( { interface => 'worker' } )
