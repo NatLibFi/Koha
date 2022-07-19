d19ce1e9f434706a01ca8e92f3dd1a21516f8a41
Andrew Nugged <nugged@gmail.com>
2022-07-20 00:48:58 +0300

    Bug 00000: HOTFIX. Does those $dbh needed?
    NOTE: there are MUCH more in the code, search in Sublime Text by regex:
    
        (?ms)my\s+\$dbh\b(.(?!\$dbh\b))+\Z

--- C4/Biblio.pm
+++ C4/Biblio.pm
@@ -2855,7 +2855,6 @@ sub ModBiblioMarc {
 
     # Clone record as it gets modified
     $record = $record->clone();
-    my $dbh    = C4::Context->dbh;
     my @fields = $record->fields();
     my $encoding = C4::Context->preference("marcflavour");
 
