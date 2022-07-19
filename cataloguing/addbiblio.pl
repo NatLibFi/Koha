653d18b67dc0325cfa4de334b0af2f1e31f3cd4a
Andrew Nugged <nugged@gmail.com>
2022-07-20 00:31:32 +0300

    Bug 00000: HOTFIX. Warn removal: undef frameworkcode
    Use of uninitialized value $frameworkcode in string eq at /usr/share/koha/intranet/cgi-bin/cataloguing/addbiblio.pl line 754.
    Use of uninitialized value $frameworkcode in string eq at /usr/share/koha/intranet/cgi-bin/cataloguing/addbiblio.pl line 758.
    Use of uninitialized value $frameworkcode in string eq at /usr/share/koha/intranet/cgi-bin/cataloguing/addbiblio.pl line 776.

--- cataloguing/addbiblio.pl
+++ cataloguing/addbiblio.pl
@@ -526,11 +526,11 @@ my $changed_framework = $input->param('changed_framework') // q{};
 $frameworkcode = &GetFrameworkCode($biblionumber)
   if ( $biblionumber and not( defined $frameworkcode) and $op ne 'addbiblio' );
 
-if ($frameworkcode eq 'FA'){
+if ( $frameworkcode and $frameworkcode eq 'FA' ){
     $userflags = 'fast_cataloging';
 }
 
-$frameworkcode = '' if ( $frameworkcode eq 'Default' );
+$frameworkcode = '' if ( $frameworkcode and $frameworkcode eq 'Default' );
 my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
     {
         template_name   => "cataloguing/addbiblio.tt",
@@ -549,7 +549,7 @@ if ($biblionumber){
     }
 }
 
-if ($frameworkcode eq 'FA'){
+if ($frameworkcode and $frameworkcode eq 'FA'){
     # We need to grab and set some variables in the template for use on the additems screen
     $template->param(
         'circborrowernumber' => $fa_circborrowernumber,
