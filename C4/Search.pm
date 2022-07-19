20aaab3b1fc6a1317630a5eb54f9ecf092eea1f6
Andrew Nugged <nugged@gmail.com>
2022-07-20 00:48:58 +0300

    Bug 00000: HOTFIX. Does those $dbh needed?
    NOTE: there are MUCH more in the code, search in Sublime Text by regex:
    
        (?ms)my\s+\$dbh\b(.(?!\$dbh\b))+\Z

--- C4/Search.pm
+++ C4/Search.pm
@@ -88,7 +88,6 @@ This function attempts to find duplicate records using a hard-coded, fairly simp
 
 sub FindDuplicate {
     my ($record) = @_;
-    my $dbh      = C4::Context->dbh;
     my $result   = TransformMarcToKoha( { record => $record } );
     my $sth;
     my $query;
@@ -1659,7 +1658,6 @@ Format results in a form suitable for passing to the template
 # building the HTML output for the template
 sub searchResults {
     my ( $search_context, $searchdesc, $hits, $results_per_page, $offset, $scan, $marcresults, $xslt_variables ) = @_;
-    my $dbh = C4::Context->dbh;
     my @newresults;
 
     require C4::Items;
