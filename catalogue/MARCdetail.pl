f226be5efcf98b532f001c5949ea77ab6666cf5a
Andrew Nugged <nugged@gmail.com>
2022-08-01 20:10:11 +0300

    Bug 00000: HOTFIX. Warn removal: MARCdetail.pl: m//, eq, ne with undef in keys: kohafield, tab, hidden
    Use of uninitialized value in string ne at .../catalogue/MARCdetail.pl line 175.
    Use of uninitialized value in string ne at .../catalogue/MARCdetail.pl line 194.
    Use of uninitialized value in pattern match (m//) at .../catalogue/MARCdetail.pl line 197.
    Use of uninitialized value in string eq at .../catalogue/MARCdetail.pl line 212.
    Use of uninitialized value in string ne at .../catalogue/MARCdetail.pl line 283.

--- catalogue/MARCdetail.pl
+++ catalogue/MARCdetail.pl
@@ -167,7 +167,7 @@ for ( my $tabloop = 0 ; $tabloop <= 10 ; $tabloop++ ) {
         # if tag <10, there's no subfield, use the "@" trick
         if ( $fields[$x_i]->tag() < 10 ) {
             next
-              if (
+              if ( not defined $tagslib->{ $fields[$x_i]->tag() }->{'@'}->{tab} or
                 $tagslib->{ $fields[$x_i]->tag() }->{'@'}->{tab} ne $tabloop );
             next if ( $tagslib->{ $fields[$x_i]->tag() }->{'@'}->{hidden} =~ /-7|-4|-3|-2|2|3|5|8/);
             my %subfield_data;
@@ -186,7 +186,8 @@ for ( my $tabloop = 0 ; $tabloop <= 10 ; $tabloop++ ) {
                 $subf[$i][0] = "@" unless defined $subf[$i][0];
                 next if ( $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }->{tab} // q{} ) ne $tabloop; # Note: defaulting to '0' changes behavior!
                 next
-                  if ( $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }
+                  if ( defined $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }->{hidden} and
+                    $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }
                     ->{hidden} =~ /-7|-4|-3|-2|2|3|5|8/);
                 my %subfield_data;
                 $subfield_data{short_desc} = $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }->{lib};
@@ -202,7 +203,8 @@ for ( my $tabloop = 0 ; $tabloop <= 10 ; $tabloop++ ) {
                     $subfield_data{marc_value} = $subf[$i][1];
 					$subfield_data{is_url} = 1;
                 }
-                elsif ( $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }
+                elsif ( defined $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }->{kohafield} and
+                        $tagslib->{ $fields[$x_i]->tag() }->{ $subf[$i][0] }
                     ->{kohafield} eq "biblioitems.isbn" )
                 {
 
@@ -271,8 +273,10 @@ foreach my $field (@fields) {
 
     # loop through each subfield
     for my $i ( 0 .. $#subf ) {
-        next if ( $tagslib->{ $field->tag() }->{ $subf[$i][0] }->{tab} ne 10 );
-        next if ( $tagslib->{ $field->tag() }->{ $subf[$i][0] }->{hidden} =~ /-7|-4|-3|-2|2|3|5|8/);
+        next if ( defined $tagslib->{ $field->tag() }->{ $subf[$i][0] }->{tab}
+            and $tagslib->{ $field->tag() }->{ $subf[$i][0] }->{tab} ne 10 );
+        next if ( defined $tagslib->{ $field->tag() }->{ $subf[$i][0] }->{hidden}
+            and $tagslib->{ $field->tag() }->{ $subf[$i][0] }->{hidden} =~ /-7|-4|-3|-2|2|3|5|8/);
 
         push @item_subfield_codes, $subf[$i][0];
         $witness{ $subf[$i][0] } =
