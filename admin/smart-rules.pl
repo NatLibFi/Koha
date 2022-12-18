ec65a41cdb15f0c7e3f04276b5a012a3ed93142c
Andrew Nugged <nugged@gmail.com>
2022-12-18 14:18:54 +0200

    Bug 00000: prevent CGI warnings

--- admin/smart-rules.pl
+++ admin/smart-rules.pl
@@ -261,7 +261,7 @@ elsif ($op eq 'add') {
     my $itemtype  = $input->param('itemtype');     # item type
     my $ccode             = $input->param('ccode');
     my $shelving_location = $input->param('shelving_location');
-    my $fine = Koha::Number::Price->new( $input->param('fine') )->unformat();
+    my $fine = Koha::Number::Price->new( scalar $input->param('fine') )->unformat();
     my $finedays     = $input->param('finedays');
     my $maxsuspensiondays = $input->param('maxsuspensiondays') || q{};
     my $suspension_chargeperiod = $input->param('suspension_chargeperiod') || 1;
@@ -289,7 +289,7 @@ elsif ($op eq 'add') {
     my $hardduedate = $input->param('hardduedate') || q{};
     my $hardduedatecompare = $input->param('hardduedatecompare');
     my $rentaldiscount = $input->param('rentaldiscount')
-        && Koha::Number::Price->new( $input->param('rentaldiscount') )->unformat()
+        && Koha::Number::Price->new( scalar $input->param('rentaldiscount') )->unformat()
         || 0;
     my $opacitemholds = $input->param('opacitemholds') || 0;
     my $article_requests = $input->param('article_requests') || 'no';
@@ -303,7 +303,7 @@ elsif ($op eq 'add') {
     my $recalls_per_record = $input->param('recalls_per_record');
     my $on_shelf_recalls = $input->param('on_shelf_recalls');
     my $recall_due_date_interval = $input->param('recall_due_date_interval');
-    my $recall_overdue_fine = Koha::Number::Price->new( $input->param('recall_overdue_fine') )->unformat();
+    my $recall_overdue_fine = Koha::Number::Price->new( scalar $input->param('recall_overdue_fine') )->unformat();
     my $recall_shelf_time = $input->param('recall_shelf_time');
 
     my $rules = {
