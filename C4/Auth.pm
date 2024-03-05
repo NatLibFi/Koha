c61c1672eb9b492aa8dbacbea418b59c3ebe2f41
Petro Vashchuk <stalkernoid@gmail.com>
2024-03-05 18:16:47 +0200

    WIP: Append default framework to the default holdings framework

--- C4/Auth.pm
+++ C4/Auth.pm
@@ -1298,6 +1298,7 @@ sub checkauth {
                     $session->param('emailaddress'), $session->param('shibboleth'),
                     $session->param('desk_id'),      $session->param('desk_name'),
                     $session->param('register_id'),  $session->param('register_name'),
+                    $session->param('default_framework'),
                     $session->param('default_holding_framework'),
                 );
 
@@ -1756,6 +1757,7 @@ sub check_api_auth {
                 $session->param('emailaddress'), $session->param('shibboleth'),
                 $session->param('desk_id'),      $session->param('desk_name'),
                 $session->param('register_id'),  $session->param('register_name'),
+                $session->param('default_framework'),
                 $session->param('default_holding_framework'),
             );
             return ( "ok", $cookie, $sessionID );
@@ -1883,6 +1885,7 @@ sub check_cookie_auth {
                     $session->param('emailaddress'), $session->param('shibboleth'),
                     $session->param('desk_id'),      $session->param('desk_name'),
                     $session->param('register_id'),  $session->param('register_name'),
+                    $session->param('default_framework'),
                     $session->param('default_holding_framework'),
                 );
                 if ( C4::Context->preference('TwoFactorAuthentication') ne 'disabled' ) {
