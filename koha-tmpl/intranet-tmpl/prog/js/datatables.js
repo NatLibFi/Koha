bd257a12f0b54924ea33575a56fc3ba66a09f426
Andrii Nugged <nugged@gmail.com>
2024-03-22 15:33:44 +0200

    Bug 36291: HOTFIX - disable fixed header

--- koha-tmpl/intranet-tmpl/prog/js/datatables.js
+++ koha-tmpl/intranet-tmpl/prog/js/datatables.js
@@ -50,7 +50,7 @@ var dataTablesDefaults = {
     }],
     "lengthMenu": [[10, 20, 50, 100, -1], [10, 20, 50, 100, __('All')]],
     "pageLength": 20,
-    "fixedHeader": true,
+    "fixedHeader": typeof global_fixedHeaderSetting !== 'undefined' ? global_fixedHeaderSetting : true,
     initComplete: function( settings ) {
         var tableId = settings.nTable.id
         var state =  settings.oLoadedState;
