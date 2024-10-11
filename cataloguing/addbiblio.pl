4f4d6d5fa442d29f160486d9c8e99f99c91d4d73
Andrii Nugged <nugged@gmail.com>
2024-10-11 04:14:35 +0300

    WIP: Prevent warning on undef $frameworkcode

--- cataloguing/addbiblio.pl
+++ cataloguing/addbiblio.pl
@@ -529,11 +529,11 @@ my ( $template, $loggedinuser, $cookie ) = get_template_and_user(
 $frameworkcode = &GetFrameworkCode($biblionumber)
   if ( $biblionumber and not( defined $frameworkcode) and $op ne 'cud-addbiblio' );
 
-if ($frameworkcode eq 'FA'){
+if ($frameworkcode and $frameworkcode eq 'FA'){
     $userflags = 'fast_cataloging';
 }
 
-$frameworkcode = '' if ( $frameworkcode eq 'Default' );
+$frameworkcode = '' if ( $frameworkcode and $frameworkcode eq 'Default' );
 
 # Set default values for global variable
 $tagslib           = &GetMarcStructure( 1, $frameworkcode );
@@ -568,7 +568,7 @@ if ($biblionumber) {
     }
 }
 
-if ($frameworkcode eq 'FA'){
+if ($frameworkcode and $frameworkcode eq 'FA'){
     # We need to grab and set some variables in the template for use on the additems screen
     $template->param(
         'circborrowernumber' => $fa_circborrowernumber,
