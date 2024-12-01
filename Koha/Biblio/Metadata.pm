84be7dd8c2da63605dd13d16489677e0147ff27d
Andrii Nugged <nugged@gmail.com>
2024-12-02 00:45:50 +0200

    Bug 20447: MARC Holdings support
    Enable by setting "SummaryHoldings" option to "Use".
    
    Smaller notes to task parts (batch rebuild script):
    ---
    
    Includes misc/batchRebuildHoldingsTables.pl: a batch rebuild script for
    holdings table which can be used to update the columns in the holdings
    table from the MARC record using the rules in the framework. In
    practice this will only be required if the koha link field is changed
    in the framework.
    
    ---
    End of smaller notes
    
    Sponsored-by: The National Library of Finland
    
    Co-authored-by: Joonas Kylmälä <joonas.kylmala@helsinki.fi>
    Co-authored-by: Andrew Nugged <nugged@gmail.com>
    
    Signed-off-by: Martin Renvoize <martin.renvoize@ptfs-europe.com>
    
    Signed-off-by: Nick Clemens <nick@bywatersolutions.com>

--- Koha/Biblio/Metadata.pm
+++ Koha/Biblio/Metadata.pm
@@ -55,25 +55,27 @@ corresponds to this table:
 
     $record = $biblio->metadata->record({
         {
-            embed_items => 0|1
-            itemnumbers => $itemnumbers,
-            opac        => $opac
+            embed_items   => 0|1
+            itemnumbers   => $itemnumbers,
+            opac          => $opac,
+            skip_holdings => 1
         }
     );
 
     Koha::Biblio::Metadata::record(
         {
-            record       => $record,
-            embed_items  => 1,
-            biblionumber => $biblionumber,
-            itemnumbers  => $itemnumbers,
-            opac         => $opac
+            record        => $record,
+            embed_items   => 1,
+            biblionumber  => $biblionumber,
+            itemnumbers   => $itemnumbers,
+            opac          => $opac,
+            skip_holdings => 1
         }
     );
 
 Given a MARC::Record object containing a bib record,
 modify it to include the items attached to it as 9XX
-per the bib's MARC framework.
+per the bib's MARC framework and any holdings location information.
 if $itemnumbers is defined, only specified itemnumbers are embedded.
 
 If $opac is true, then opac-relevant suppressions are included.
@@ -81,6 +83,9 @@ If $opac is true, then opac-relevant suppressions are included.
 If opac filtering will be done, patron should be passed to properly
 override if necessary.
 
+If $skip_holdings is set, it overrides the default of embedding basic
+location information from holdings records if summary holdings are
+enabled.
 
 =head4 Error handling
 
@@ -100,6 +105,7 @@ sub record {
 
     my $record      = $params->{record};
     my $embed_items = $params->{embed_items};
+    my $skip_holdings = $params->{skip_holdings};
     my $format      = blessed($self) ? $self->format : $params->{format};
     $format ||= 'marcxml';
 
@@ -134,7 +140,7 @@ sub record {
     $record->delete_field( ( $record->field($itemtag) ) );
 
     if ($embed_items) {
-        $self->_embed_items( { %$params, format => $format, record => $record } );
+        $self->_embed_items( { %$params, format => $format, record => $record, skip_holdings => $skip_holdings } );
     }
 
     return $record;
@@ -246,6 +252,13 @@ sub _embed_items {
         $record->insert_fields_ordered( reverse @item_fields );
 
         # insert_fields_ordered with the reverse keeps 952s in right order
+
+        my $skip_holdings = $params->{skip_holdings} // 0;
+        if ( !$skip_holdings && C4::Context->preference('SummaryHoldings') && !@$itemnumbers ) {
+            my $holdings_fields = Koha::Holdings->get_embeddable_marc_fields( { biblionumber => $biblionumber } );
+            $record->insert_fields_ordered(@$holdings_fields)
+                if @$holdings_fields;
+        }
 
     } else {
         Koha::Exceptions::Metadata->throw(
